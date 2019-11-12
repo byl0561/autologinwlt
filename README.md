2519acd2e4284a01f4983154d8ebef95c458426d
鲍裕麟 <byl0561@gmail.com>
2019-11-12 23:49:01 +0800

    ver. 4.0.0-r7

--- README.md
+++ README.md
@@ -16,18 +16,32 @@ Auto Login WLS is deployed in the docker environment as default. In fact, you ca
 
 ### Write name and password as environment variables
 
-Replace the original account and password with your personal campus ones in Dockerfile.
+Replace the original account and password with your personal campus ones in Dockerfile, then set the port.
+
+|port|网络出口|
+|:-:|:-:|
+|0|1教育网出口(国际,仅用教育网访问,适合看文献)|
+|1|2电信网出口(国际,到教育网走教育网)|
+|2|3联通网出口(国际,到教育网走教育网)|
+|3|4电信网出口2(国际,到教育网免费地址走教育网)|
+|4|5联通网出口2(国际,到教育网免费地址走教育网)|
+|5|6电信网出口3(国际,默认电信,其他分流)|
+|6|7联通网出口3(国际,默认联通,其他分流)|
+|7|8教育网出口2(国际,默认教育网,其他分流)|
+|8|9移动网出口(国际,无P2P或带宽限制)|
 
 from
 ```
-ENV name
-ENV pass
+	ENV name
+	ENV pass
+	ENV port
 ```
 
 to
 ```
-ENV name "campus account"
-ENV pass "campus password"
+	ENV name "campus account"
+	ENV pass "campus password"
+	ENV port "0-8"
 ```
 
 ### Build and Run docker image
@@ -35,6 +49,14 @@ ENV pass "campus password"
 To build and run docker image, please refer to [docker project](https://www.docker.com/), or you could also run autoLoginWLS.sh in any environment.
 
 ## History
+
+### 4.0.0-r7
+
+* Added ENV port, now it is supported to choose the network export.
+
+* Reduce the SLEEP time in code, now it reconnects faster.
+
+* Added Init code to show ENV settings.
 
 ### 3.1.0-r6
 
--- autologinwls/autoLoginWLT.sh
+++ autologinwls/autoLoginWLT.sh
@@ -4,7 +4,6 @@ check(){
   ping -c 2 -w 5 www.baidu.com > /dev/null 2>&1
   if [[ $? != 0 ]];then
     echo "Ping fail, try again..."
-	sleep 10
 	ping -c 4 -w 5 www.baidu.com > /dev/null 2>&1
 	if [[ $? != 0 ]];then
       echo "Ping fail"
@@ -12,7 +11,6 @@ check(){
 	  while [[ $? != 0 ]]
 	  do
 	    echo "Connect fail, retrying..."
-	    sleep 5
 	    connect
 	  done
 	  echo "Connect success"
@@ -36,7 +34,6 @@ connect(){
 	  return 1
 	fi
 	echo "Access success"
-	sleep 1
   fi
   ip=$(grep -o -m 1 "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" getIP)
   curl http://202.38.64.59/cgi-bin/ip -X POST -H "application/x-www-form-urlencoded" -d "cmd=login&url=URL&ip=${ip}&name=${name}&password=${pass}&set=%B5%C7%C2%BC%D5%CA%BB%A7" -m 10  -o getCookie -s -i
@@ -50,12 +47,11 @@ connect(){
 	  return 1
 	fi
 	echo "Login success"
-	sleep 1
   fi
   cookie=$(grep "^Set-Cookie: rn=" getCookie)
   cookie=${cookie#*=}
   cookie=${cookie:0:40}
-  curl http://202.38.64.59/cgi-bin/ip?cmd=set\&url=URL\&type=7\&exp=0\&go=+%BF%AA%CD%A8%CD%F8%C2%E7+ --cookie "name=; password=; rn=$cookie" -m 10 -o result -s
+  curl http://202.38.64.59/cgi-bin/ip?cmd=set\&url=URL\&type=$port\&exp=0\&go=+%BF%AA%CD%A8%CD%F8%C2%E7+ --cookie "name=; password=; rn=$cookie" -m 10 -o result -s
   if [[ $? != 0 ]];then
 	echo "Network authentication fail"
 	return 1
@@ -69,12 +65,50 @@ connect(){
 	return 1
   else
 	echo "Ping success"
-	sleep 1
   fi
 }
 
-main(){
+init(){
   echo "autoLoginWLT 3.1.0-r6 starts"
+  echo "username: $name"
+  echo "password: $pass"
+  case $port in
+    0)
+        echo "出口：教育网出口(国际,仅用教育网访问,适合看文献)"
+        ;;
+    1)
+        echo "出口：电信网出口(国际,到教育网走教育网)"
+        ;;
+    2)
+        echo "出口：联通网出口(国际,到教育网走教育网)"
+        ;;
+    3)
+        echo "出口：电信网出口2(国际,到教育网免费地址走教育网)"
+        ;;
+	4)
+        echo "出口：联通网出口2(国际,到教育网免费地址走教育网)"
+        ;;
+    5)
+        echo "出口：电信网出口3(国际,默认电信,其他分流)"
+        ;;
+    6)
+        echo "出口：联通网出口3(国际,默认联通,其他分流)"
+        ;;
+    7)
+        echo "出口：教育网出口2(国际,默认教育网,其他分流)"
+        ;;
+	8)
+        echo "出口：移动网出口(国际,无P2P或带宽限制)"
+        ;;
+    *)
+        echo "出口：出口参数错误，使用默认电信网出口"
+		port=1
+		;;
+  esac
+}
+
+main(){
+  init
   while true
   do
     check
